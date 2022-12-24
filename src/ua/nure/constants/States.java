fef2e7d8c75dac7c451fee93c709a66986ec595a
Daniel Didkovskiy <didkovskiythebest@gmail.com>
2022-12-24 21:07:56 +0200

    Add files via upload

---
+++ src/ua/nure/constants/States.java
@@ -0,0 +1,13 @@
+package ua.nure.constants;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public final class States {
+    public final static Map<Integer, String> STATES = new LinkedHashMap<>() {{
+        put(1, "Stench");
+        put(2, "Breeze");
+        put(3, "Glitter");
+        put(4, "Scream");
+    }};
+}
